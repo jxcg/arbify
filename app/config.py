7f2d88d755e0ebed1d731f15a135576e40779e1c
jxcg <154761377+jxcg@users.noreply.github.com>
2025-06-19 00:27:35 +0100

    Migrate to Streamlit

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://arbuser:temp_secure_44@143.47.226.107:5432/arbifydb")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "jwt-secret")
